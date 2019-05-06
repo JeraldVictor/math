f51e8e14c12d3b82ebd03129a34d94209257a910
Jerald Victor <jerald@Jeralds-MacBook-Air.local>
2019-05-06 11:01:40 +0530

    v1.0

--- app.js
+++ app.js
@@ -1,39 +1,67 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const session = require('express-session');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
 
-
-const indexRouter = require('./routes/index');
-const update = require('./routes/update');
-const api = require('./routes/api');
+const indexRouter = require("./routes/index");
+const update = require("./routes/update");
+const api = require("./routes/api");
 const app = express();
 
+var MySQLStore = require("express-mysql-session")(session);
+
+// var options = {
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '',
+//     database: 'anthony'
+// };
+var options = {
+  host: "localhost",
+  port:3306,
+  user: "jeraldvi_jerald",
+  password: "Johnpeter@17",
+  database: "jeraldvi_anthony"
+};
+var sessionStore = new MySQLStore(options);
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
-  secret: 'JeraldVictor',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 10000000 }
-}));
+app.set("trust proxy", 1);
+// app.use(session({
+//   secret: 'JeraldVictor',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { maxAge: 10000000 }
+// }));
+
+app.use(
+  session({
+    key: "session_cookie_name",
+    secret: "JeraldVictor",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 //bootstrap
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstra
-app.use('/css/ui', express.static(__dirname + '/node_modules/jquery-ui-dist'))
-app.use('/js/ui', express.static(__dirname + '/node_modules/jquery-ui-dist'))
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js")); // redirect bootstrap JS
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist")); // redirect JS jQuery
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // redirect CSS bootstra
+app.use("/css/ui", express.static(__dirname + "/node_modules/jquery-ui-dist"));
+app.use("/js/ui", express.static(__dirname + "/node_modules/jquery-ui-dist"));
 
-app.use('/', indexRouter);
-app.use('/update', update);
-app.use('/api', api);
+app.use("/", indexRouter);
+app.use("/update", update);
+app.use("/api", api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,11 +72,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/db.js
+++ routes/db.js
@@ -1,25 +1,28 @@
 const mysql      = require('mysql');
 
+// const connection = mysql.createConnection({
+//   // host     : '192.168.1.5',
+//   host     :"localhost",
+//   port:3306,
+//   user     : 'root',
+//   password : '',
+//   database : 'anthony'
+// });
+
 const connection = mysql.createConnection({
-  // host     : '192.168.1.5',
-  host     :"localhost",
+  host     : 'localhost',
   port:3306,
-  user     : 'root',
-  password : '',
-  database : 'anthony'
-},(err,list)=>{
-  if(err){console.log(errr)}else{
-    console.log("connected ",list)
+  user     : 'jeraldvi_jerald',
+  password : 'Johnpeter@17',
+  database : 'jeraldvi_anthony'
+});
+ 
+connection.connect((err)=>{
+  if(err){
+    console.log(err)
+  }else{
+    console.log("ok mysql connected")
   }
 });
-
-// const connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'jeraldvi_jerald',
-//   password : 'Johnpeter@17',
-//   database : 'jeraldvi_anthony'
-// });
- 
-connection.connect();
 
 module.exports = connection;
